3937b9998811e283e7c77a904a600cee9d896166
Greg <gvoakes@gmail.com>
2025-05-11 16:16:52 -0700

    Delete client directory

--- client/src/lib/toast-types.ts
+++
@@ -1,9 +0,0 @@
-// Custom type definitions for our toast system
-export type ToastType = 'success' | 'error' | 'info';
-
-export interface CustomToastProps {
-  title?: string;
-  description?: string;
-  type?: ToastType;
-  duration?: number;
-}
